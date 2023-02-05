be41360f493a1f8a0300f3f7200322ffe0db75ca
Anri Sakakibara <anrisakaki@gmail.com>
2023-02-05 22:04:08 +0000

    Regression on labour market outcomes

--- Regressions on labour market outcomes.R
+++ Regressions on labour market outcomes.R
@@ -3,12 +3,15 @@
 ##############################
 
 LFS_2017 <- LFS_2017 %>% 
-  mutate(year = 2017) %>% 
-  mutate(across(educ, as.numeric))
+  mutate(year = 2017,
+         across(educ, as.numeric))
 
 LFS_2019 <- LFS_2019 %>% 
   mutate(year = 2019,
-         treated = ifelse(is.na(treated), 0 , treated))
+         treated = ifelse(is.na(treated), 0 , treated),
+         product_lines_affected = ifelse(is.na(product_lines_affected), 0, product_lines_affected),
+         nb_of_lines = ifelse(is.na(nb_of_lines), 0, nb_of_lines),
+         share_HS_lines_affected = ifelse(is.na(share_HS_lines_affected), 0, share_HS_lines_affected))
 
 LFS_1719 <- bind_rows(LFS_2017, LFS_2019)
 
@@ -17,20 +20,6 @@ LFS_1719 <- bind_rows(LFS_2017, LFS_2019)
 ################################
 
 y <- c("work", "formal", "casual_contract", "hours", "log(wage)")
-
-simple_dummy_models <- list()
-
-for(i in y){
-  formula <- as.formula(paste(i, "~ as.factor(treated) | year^month + ISIC"))
-  
-  model <- feols(formula,
-                 LFS_1719,
-                 vcov = ~ISIC,
-                 weights = ~weight)
-  
-  simple_dummy_models[[i]] <- model
-  
-}
 
 dummy_models_wcontrols <- list()
 
@@ -43,20 +32,6 @@ for(i in y){
                  weights = ~weight)
   
   dummy_models_wcontrols[[i]] <- model
-  
-}
-
-productlines_models <- list()
-
-for(i in y){
-  formula <- as.formula(paste(i, "~ product_lines_affected | year^month + ISIC"))
-  
-  model <- feols(formula,
-                 subset(LFS_1719, age > 17 & age < 65),
-                 vcov = ~ISIC,
-                 weights = ~weight)
-  
-  productlines_models[[i]] <- model
   
 }
 
@@ -74,33 +49,40 @@ for(i in y){
   
 }
 
+nb_lines_affected <- list()
+
+for(i in y){
+  formula <- as.formula(paste(i, "~ nb_of_lines + age + age^2 + educ + urban | year^month + ISIC"))
+  
+  model <- feols(formula,
+                 subset(LFS_1719, age > 17 & age < 65),
+                 vcov = ~ISIC,
+                 weights = ~weight)
+  
+  nb_lines_affected[[i]] <- model
+  
+}
+
+share_HS_lines_affected <- list()
+
+for(i in y){
+  formula <- as.formula(paste(i, "~ share_HS_lines_affected + age + age^2 + educ + urban | year^month + ISIC"))
+  
+  model <- feols(formula,
+                 subset(LFS_1719, age > 17 & age < 65),
+                 vcov = ~ISIC,
+                 weights = ~weight)
+  
+  share_HS_lines_affected[[i]] <- model
+  
+}
+
+
 ###################
 # MODEL SUMMARIES #
 ###################
 
-etable(list(
-  simple_dummy_models[[1]],
-  simple_dummy_models[[2]],
-  simple_dummy_models[[3]],
-  simple_dummy_models[[4]],
-  simple_dummy_models[[5]]
-), tex = TRUE)
-
-etable(list(
-  dummy_models_wcontrols[[1]],
-  dummy_models_wcontrols[[2]],
-  dummy_models_wcontrols[[3]],
-  dummy_models_wcontrols[[4]],
-  dummy_models_wcontrols[[5]]
-), tex = TRUE)
-
-etable(list(
-  productlines_models[[1]],
-  productlines_models[[2]],
-  productlines_models[[3]],
-  productlines_models[[4]],
-  productlines_models[[5]]
-), tex = TRUE)
+etable(dummy_models_wcontrols)
 
 etable(list(
   productlines_models_controls[[1]],
@@ -108,4 +90,8 @@ etable(list(
   productlines_models_controls[[3]],
   productlines_models_controls[[4]],
   productlines_models_controls[[5]]
-), tex = TRUE)
+))
+
+etable(nb_lines_affected)
+
+etable(share_HS_lines_affected)
