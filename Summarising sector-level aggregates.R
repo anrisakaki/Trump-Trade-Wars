0857b291feb4c6718b92189e5eaf0ffdf5b0a466
Anri Sakakibara <anrisakaki@gmail.com>
2023-02-19 22:11:46 +0000

    Summarising sector-level aggregates

--- Summarising sector-level aggregates.R
+++ Summarising sector-level aggregates.R
@@ -1,6 +1,37 @@
 ######################################
 # SUMMARISING SECTOR-LEVEL VARIABLES #
 ######################################
+# 2017
+female_sector_17 <- LFS_2017 %>% 
+  filter(work == 1) %>% 
+  select(ISIC, Female, FDI, weight, month) %>% 
+  group_by(ISIC, month) %>% 
+  count(Female, wt = weight) %>% 
+  filter(Female == 1) %>% 
+  select(-c(Female)) %>% 
+  rename(n_female = n)
+
+lfp_sector_17 <- LFS_2017 %>% 
+  filter(work == 1) %>% 
+  select(ISIC, month, work, weight) %>% 
+  group_by(ISIC, month) %>% 
+  count(work, wt = weight) %>% 
+  select(-c(work)) %>% 
+  rename(n_workers = n)
+
+formal_sector_17 <- LFS_2017 %>% 
+  filter(work == 1) %>% 
+  select(ISIC, month, formal, weight) %>% 
+  group_by(ISIC, month) %>% 
+  count(formal, wt = weight) %>% 
+  filter(formal == 1) %>% 
+  select(-c(formal)) %>% 
+  rename(n_formal = n)
+
+sector_17 <- list(female_sector_17, lfp_sector_17, formal_sector_17) %>% 
+  reduce(full_join, by = c("ISIC", "month")) %>% 
+  mutate(female_ratio = n_female/n_workers,
+         formal_ratio = n_formal/n_workers)
 
 # 2018 
 female_sector_18 <- LFS_2018 %>% 
