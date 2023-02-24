dd39b4cf864cb4e7f180b74c9938c726b5fd978d
Anri Sakakibara <anrisakaki@gmail.com>
2023-02-24 17:37:16 +0000

    Concordance

--- Concordance.R
+++ Concordance.R
@@ -3,27 +3,25 @@
 ##############################
 us_chn_tariffs_18 <- us_tariffs %>% 
   filter(iso3 == "CHN",
-         year == 2018) %>% 
+         year == 2018,
+         effective_mdate != 716) %>% 
   select(hs10, tariff_max, tariff_scaled, effective_mdate) %>% 
-  mutate(month = recode(effective_mdate,
-                        "704" = "9",
-                        "702" = "2",
-                        "298" = "3",
-                        "702" = "7",
-                        "703" = "8")) %>% 
-  filter(!is.na(month))
+  mutate(month = recode(effective_mdate, 
+                        '697' = '2',
+                        '698' = '3',
+                        '702' = '7',
+                        '703' = '8',
+                        '704' = '9'
+                        ),
+         treated = 1)
 
 us_chn_tariffs_19 <- us_tariffs %>% 
   filter(iso3 == "CHN",
          year == 2019) %>% 
   select(hs10, tariff_max, tariff_scaled, effective_mdate) %>% 
   mutate(month = recode(effective_mdate,
-                        "716" = "9",
-                        "704" = "2018_9",
-                        "702" = "2018_2",
-                        "298" = "2018_3",
-                        "702" = "2018_7",
-                        "703" = "2018_8"))  
+                        "716" = "9"),
+         treated = 1)
 
 #####################################
 # CONVERTING ISIC4 IN LFS TO ISIC 3 #
@@ -35,14 +33,24 @@ us_chn_tariffs_19$HS6 <- as.numeric(substr(as.character(us_chn_tariffs_19$hs10),
 trump_tariffs <- c("us_chn_tariffs_18", "us_chn_tariffs_19")
 
 for(i in trump_tariffs){
-  assign(i, merge(get(i), hs_i3, by = "HS6"))
-  
-  assign(i, merge(get(i), i4_i3, by = "ISIC"))
   
   assign(i, get(i) %>% 
+           select(-"hs10"))
+  
+  assign(i, merge(get(i), hs_i3, by = "HS6"))
+  
+  assign(i, get(i) %>%
+    select(tariff_max, tariff_scaled, effective_mdate, month, ISIC, treated) %>%
+    group_by(effective_mdate, ISIC) %>%
+    distinct()) # ISIC in this dataframe is ISIC3
+
+  assign(i, merge(get(i), i4_i3, by = "ISIC")) %>%
+    group_by(effective_mdate, ISIC4) %>%
+    distinct()
+
+  assign(i, get(i) %>%
            rename(ISIC3 = ISIC,
-                  ISIC = ISIC4) %>% 
-           select(ISIC3, HS6, tariff_max, tariff_scaled, effective_mdate, month, ISIC) %>% 
+                  ISIC = ISIC4) %>%
            mutate(across(month, as.numeric)))
 }
 
