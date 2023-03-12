17559b2c5c5e8d3d7a7e28586acb10e4515bda51
Anri Sakakibara <anrisakaki@gmail.com>
2023-03-12 21:21:45 +0000

    Summarising sector-level aggregates

--- Summarising sector-level aggregates.R
+++ Summarising sector-level aggregates.R
@@ -31,7 +31,9 @@ formal_sector_17 <- LFS_2017 %>%
 sector_17 <- list(female_sector_17, lfp_sector_17, formal_sector_17) %>% 
   reduce(full_join, by = c("ISIC", "month")) %>% 
   mutate(female_ratio = n_female/n_workers,
-         formal_ratio = n_formal/n_workers)
+         formal_ratio = n_formal/n_workers,
+         year = 2017,
+         year_month = year*100 + month)
 
 # 2018 
 female_sector_18 <- LFS_2018 %>% 
@@ -63,7 +65,9 @@ formal_sector_18 <- LFS_2018 %>%
 sector_18 <- list(female_sector_18, lfp_sector_18, formal_sector_18) %>% 
   reduce(full_join, by = c("ISIC", "month")) %>% 
   mutate(female_ratio = n_female/n_workers,
-         formal_ratio = n_formal/n_workers)
+         formal_ratio = n_formal/n_workers,
+         year = 2018,
+         year_month = year*100 + month)
 
 # 2019 
 female_sector_19 <- LFS_2019 %>% 
@@ -95,4 +99,6 @@ formal_sector_19 <- LFS_2019 %>%
 sector_19 <- list(female_sector_19, lfp_sector_19, formal_sector_19) %>% 
   reduce(full_join, by = c("ISIC", "month")) %>% 
   mutate(female_ratio = n_female/n_workers,
-         formal_ratio = n_formal/n_workers)
+         formal_ratio = n_formal/n_workers,
+         year = 2019,
+         year_month = year*100 + month)
