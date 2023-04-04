f1de61a65c89e0b307e9efb4cff02882c29cc8a3
Anri Sakakibara <anrisakaki@gmail.com>
2023-04-04 15:59:58 +0100

    Individual-level static and dynamic TWFE

--- Individual-level static and dynamic TWFE.R
+++ Individual-level static and dynamic TWFE.R
@@ -9,10 +9,83 @@ for (i in LFS_1520){
                                          '703' = '201808',
                                          '704' = '201809',
                                          '716' = '201909'),
-                  treated = ifelse(year_month > first_treated, 1, 0),
+                  treat = ifelse(year_month > first_treated, 1, NA),
                   first_treated = ifelse(is.na(first_treated), 0, first_treated),
-                  treated = ifelse(is.na(treated), 0, treated),
-                  treat = ifelse(first_treated > 0, 1, 0)))
+                  treat = ifelse(is.na(treat), 0, treat),
+                  treated = ifelse(first_treated > 0, 1, 0)))
 }
 
-LFS1520 <- bind_rows()
+LFS1520 <- bind_rows(list(LFS_2015, LFS_2016, LFS_2017, LFS_2018, LFS_2019, LFS_2020))
+
+LFS1520$year_ft <- as.numeric(substr(trimws(format(LFS1520$first_treated, scientific = F)), 1, 4))
+
+###############
+# STATIC TWFE #
+###############
+
+etable(list(
+  feols(work ~ i(year, treated, 2018) | ISIC^month + year,
+        weights = ~ weight,
+        vcov = ~ ISIC,
+        LFS1520),
+  feols(hours ~ i(year, treated, 2018) | ISIC^month + year,
+        weights = ~ weight,
+        vcov = ~ ISIC,
+        LFS1520),
+  feols(log(wage) ~ i(year, treated, 2018) | ISIC^month + year,
+        weights = ~ weight,
+        vcov = ~ ISIC,
+        LFS1520)
+), tex = T)
+
+etable(list(
+  feols(work ~ i(year, treated, 2018) + age + age^2 + educ + Female + urban | ISIC^month + year,
+        weights = ~ weight,
+        vcov = ~ ISIC,
+        LFS1520)),
+  feols(hours ~ i(year, treated, 2018) + age + age^2 + educ + Female + urban | ISIC^month + year,
+        weights = ~ weight,
+        vcov = ~ ISIC,
+        LFS1520),
+  feols(log(wage) ~ i(year, treated, 2018) + age + age^2 + educ + Female + urban | ISIC^month + year,
+        weights = ~ weight,
+        vcov = ~ ISIC,
+        LFS1520), tex = T)
+
+etable(list(
+  feols(formal ~ i(year, treat, 2018) + age + age^2 + educ + Female + urban | ISIC^month + year,
+        weights = ~ weight,
+        vcov = ~ ISIC,
+        LFS1520)),
+  feols(casual_contract ~ i(year, treat, 2018) + age + age^2 + educ + Female + urban | ISIC^month + year,
+        weights = ~ weight,
+        vcov = ~ ISIC,
+        LFS1520), tex = T)
+
+########################
+# SUN AND ABRAHAM TWFE #
+########################
+
+etable(list(
+  feols(work ~ sunab(year_ft, year) | ISIC^month + year,
+        weights = ~ weight,
+        vcov = ~ISIC,
+        LFS1520),
+  feols(hours ~ sunab(year_ft, year) | ISIC^month + year,
+        weights = ~ weight,
+        vcov = ~ISIC,
+        LFS1520),
+  feols(log(wage) ~ sunab(year_ft, year) | ISIC^month + year,
+        weights = ~ weight,
+        vcov = ~ISIC,
+        LFS1520)
+), agg = "att", tex = TRUE)
+
+etable(list(
+  feols(frormal ~ sunab(year_ft, year) | ISIC^month + year,
+        weights = ~ weight,
+        vcov = ~ISIC,
+        LFS1520),
+  feols(hours ~ sunab(year_ft, year) | ISIC^month + year, LFS1520)
+), agg = "att", tex = TRUE)
+
