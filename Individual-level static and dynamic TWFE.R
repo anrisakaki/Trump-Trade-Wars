a034960cbbd38b3f8f522f94cf32125433f13f92
Anri Sakakibara <anrisakaki@gmail.com>
2023-06-05 17:16:26 +0100

    LFS - Individual level static and dynamic TWFE

--- Individual-level static and dynamic TWFE.R
+++ Individual-level static and dynamic TWFE.R
@@ -1,32 +1,9 @@
-for (i in LFS_1520){
-  assign(i, full_join(get(i), us_chn_tariff, by = "ISIC"))
-  
-  assign(i, get(i) %>% 
-           mutate(first_treated = recode(effective_mdate, 
-                                         '697' = '201802',
-                                         '698' = '201803',
-                                         '702' = '201807',
-                                         '703' = '201808',
-                                         '704' = '201809',
-                                         '716' = '201909'),
-                  treat = ifelse(year_month > first_treated, 1, NA),
-                  treat = ifelse(is.na(treat), 0, treat),
-                  treated = ifelse(is.na(effective_mdate), 0, 1)) %>% 
-           mutate(across(month, as.numeric)))
-}
 
-LFS1520 <- bind_rows(list(LFS_2015, LFS_2016, LFS_2017, LFS_2018, LFS_2019, LFS_2020))
+setwd("C:/Users/Anri Sakakibara/OneDrive/PhD Political Economy/Trump China Trade War/")
 
-LFS1520$year_ft <- as.numeric(substr(trimws(format(LFS1520$first_treated, scientific = F)), 1, 4))
+here() # check that gives correct path
 
-LFS1520$month_hit <- as.numeric(substr(trimws(format(LFS1520$first_treated, scientific = F)), 5, 6))
-
-LFS1520 <- LFS1520 %>% 
-  mutate(ttt = year - year_ft,
-         ttt = ifelse(month < month_hit, ttt-1, ttt),
-         ttt = ifelse(treated == 0, 0, ttt),
-         ytt = year - ttt,
-         ytt = ifelse(treated == 0, 10000, ytt)) # Following Sun and Abraham, we give our never-treated units a fake "treatment" date far outside the relevant study period.
+LFS1520 <- read_dta("LFS1520_main.dta")
 
 #######################
 # BASIC EVENT STUDIES #
