18bffee6da55cb0defcf1fab53ebfbdb4d773140
Anri Sakakibara <anrisakaki@gmail.com>
2023-06-22 12:03:03 +0100

    Export data

--- Export data.R
+++
@@ -1,7 +0,0 @@
-###################################
-# SETTING UP DATA FOR FIRM EXPORT #
-###################################
-
-exp14 <- DN_2014 %>% 
-  rename(export = co_xk,
-         exp_value = tg_xktt) %>% 
