387de3417be0c54a11012a5c309961fd6b24e11b
Anri Sakakibara <anrisakaki@gmail.com>
2023-10-16 10:30:52 +0100

    Product-level TWFE

--- Product level TWFE.R
+++ Product level TWFE.R
@@ -4,34 +4,65 @@ load("dn1419.rda")
 
 dn1419 <- dn1419 %>% 
   # replacing never treated units per fixest package 
-  mutate(first_treated = ifelse(is.na(first_treated), 10000, first_treated))
+  mutate(first_treated = ifelse(willbe_treated == 0, 10000, first_treated))
 
-# event studies 
+dn1419_balanced <- dn1419_balanced %>% 
+  # replacing never treated units per fixest package 
+  mutate(first_treated = ifelse(willbe_treated == 0, 10000, first_treated))
 
-iplot(feols(log(wage) ~ i(ttt, treated17, -1)| id + year,
+
+# Event studies 
+
+## wage cost
+
+etable(list(
+  feols(log(wage) ~ i(ttt, treated17, -1)| id + year,
+        dn1419),
+  feols(log(wage) ~ i(ttt, willbe_treated, -1)| id + year,
+        dn1419)))
+
+iplot(feols(log(wage) ~ i(ttt, treated, -1)| id + year,
             dn1419), main = "Effect on labour cost")
 
-iplot(feols(log(wage) ~ i(ttt, treated17, -1)| id + year,
+### with Sun and Abraham 
+
+etable(feols(log(wage) ~ sunab(first_treated, year)| id + year,
+            dn1419))
+
+iplot(feols(log(wage) ~ sunab(first_treated, year)| id + year,
             subset(dn1419, manu == 1)), main = "Effect on labour cost")
 
-iplot(feols(log(n_workers) ~ i(ttt, treated17, -1)| id + year,
+## firm size 
+
+iplot(feols(log(n_workers) ~ i(ttt, willbe_treated, -1)| id + year,
             dn1419), main = "Effect on firm size")
 
-iplot(feols(log(n_workers) ~ i(ttt, treated17, -1)| id + year,
+iplot(feols(log(n_workers) ~ i(ttt, willbe_treated, -1)| id + year,
             subset(dn1419, manu == 1)), main = "Effect on firm size")
 
-iplot(feols(fworkers ~ i(ttt, treated17, -1)| id + year,
-            dn1419), main = "Effect on share of \nfemale workers")
+### with Sun and Abraham 
 
-iplot(feols(fworkers ~ i(ttt, treated17, -1)| id + year,
-            subset(dn1419, manu == 1)), main = "Effect on share of \nfemale workers")
+iplot(feols(log(n_workers) ~ sunab(first_treated, year)| id + year,
+            dn1419), main = "Effect on firm size")
 
-iplot(feols(export ~ i(ttt, treated, -1)| id + year,
-            dn1419), main = "Effect on exporting")
+iplot(feols(log(n_workers) ~ sunab(first_treated, year)| id + year,
+            subset(dn1419, manu == 1)), main = "Effect on firm size")
 
-iplot(feols(export ~ i(ttt, treated, -1)| id + year,
-            subset(dn1419, manu == 1)), main = "Effect on exporting")
+# Share of female workers 
+
+iplot(feols(fworkers ~ i(ttt, willbe_treated, -1)| id + year,
+            dn1419), main = "Effect on share of female workers")
+
+iplot(feols(fworkers ~ i(ttt, willbe_treated, -1)| id + year,
+            subset(dn1419, manu == 1)), main = "Effect on share of female workers")
 
 
 # switching to making new products and entering / exiting 
 
+iplot(feols(treated ~ i(ttt, willbe_treated, -1)| id + year,
+            dn1419))
+
+# Exporting 
+
+iplot(feols(export ~ i(ttt, willbe_treated, -1)| id + year,
+            dn1419))
